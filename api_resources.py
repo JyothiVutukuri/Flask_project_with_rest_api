d3a04d82364074524bdb8961cd8aaf938e5b9259
jyothi <vishnu.vutukuri@gmail.com>
2023-11-22 03:38:34 +0100

    add pagination to API

--- api_resources.py
+++ api_resources.py
@@ -1,68 +1,112 @@
-from flask_restful import Resource
-from flask import jsonify
+from flask_restful import Resource, abort
+from flask import jsonify, request
 from models import Site, Brand, Product, ProductCategory, ProductType
 
 
-class SiteResource(Resource):
+def get_paginated_list(results, url, start, limit, page_number):
+    start = int(start)
+    limit = int(limit)
+    count = len(results)
+    if count < start or limit < 0:
+        abort(404)
+    # make response
+    obj = {}
+    obj['start'] = start
+    obj['limit'] = limit
+    obj['count'] = count
+    obj['page_number'] = page_number  # Include the current page number
+    # make URLs
+    # make previous url
+    if start == 1:
+        obj['previous'] = ''
+    else:
+        start_copy = max(1, start - limit)
+        limit_copy = start - 1
+        obj['previous'] = url + '?start=%d&limit=%d' % (start_copy, limit_copy)
+    # make next url
+    if start + limit > count:
+        obj['next'] = ''
+    else:
+        start_copy = start + limit
+        obj['next'] = url + '?start=%d&limit=%d' % (start_copy, limit)
+    # finally extract result according to bounds
+    obj['results'] = results[(start - 1):(start - 1 + limit)]
+    return obj
+
+
+class PaginatedResourceBase(Resource):
+    def get_paginated_response(self, results):
+        url = request.base_url
+        start = request.args.get('start', 1)
+        limit = request.args.get('limit', 20)
+        page_number = (int(start) - 1) // int(limit) + 1  # Calculate the current page number
+        return jsonify(get_paginated_list(results, url, start, limit, page_number))
+
+
+class SiteResource(PaginatedResourceBase):
     def get(self):
         sites = Site.query.all()
-        return jsonify({'sites': [{'id': site.id, 'name': site.name, 'url': site.url} for site in sites]})
+        data = [{'id': site.id, 'name': site.name, 'url': site.url} for site in sites]
+        return self.get_paginated_response(data)
 
 
-class BrandResource(Resource):
+class BrandResource(PaginatedResourceBase):
     def get(self):
         brands = Brand.query.all()
-        return jsonify({'brands': [{'id': brand.id, 'name': brand.name} for brand in brands]})
+        data = [{'id': brand.id, 'name': brand.name} for brand in brands]
+        return self.get_paginated_response(data)
 
 
-class CategoryResource(Resource):
+class CategoryResource(PaginatedResourceBase):
     def get(self):
         categories = ProductCategory.query.all()
-        return jsonify({'categories': [{'id': category.id, 'name': category.name} for category in categories]})
+        data = [{'id': category.id, 'name': category.name} for category in categories]
+        return self.get_paginated_response(data)
 
 
-class TypeResource(Resource):
+class TypeResource(PaginatedResourceBase):
     def get(self):
         types = ProductType.query.all()
-        return jsonify({'product_types': [{'id': type.id, 'name': type.name} for type in types]})
+        data = [{'id': type.id, 'name': type.name} for type in types]
+        return self.get_paginated_response(data)
 
 
-class ProductListResource(Resource):
+class ProductListResource(PaginatedResourceBase):
     def get(self, brand_id):
         products = Product.query.filter_by(brand_id=brand_id).all()
-        return jsonify(
-            {'products': [
-                {
-                    'id': product.id,
-                    'name': product.name,
-                    'price': product.price,
-                    'page_number': product.page_number,
-                    'site': product.site.name,
-                    'brand': product.brand.name,
-                    'product_category': product.product_category.name,
-                    'product_type': product.product_type.name if product.product_type else None
-                } for product in products]
-            }
-        )
+        data = [
+            {
+                'id': product.id,
+                'name': product.name,
+                'price': product.price,
+                'page_number': product.page_number,
+                'site': product.site.name,
+                'brand': product.brand.name,
+                'product_category': product.product_category.name,
+                'product_type': product.product_type.name if product.product_type else None
+            } for product in products
+        ]
+        return self.get_paginated_response(data)
 
 
-class ProductListBySiteResource(Resource):
+class ProductListBySiteResource(PaginatedResourceBase):
     def get(self, site_id):
         products = Product.query.filter_by(site_id=site_id).all()
-        return jsonify(
-            {'products': [
-                {
-                    'id': product.id,
-                    'name': product.name,
-                    'price': product.price,
-                    'page_number': product.page_number,
-                    'site': product.site.name,
-                    'brand': product.brand.name,
-                    'product_category': product.product_category.name,
-                    'product_type': product.product_type.name if product.product_type else None
-                } for product in products]
-            }
-        )
+
+        data = [
+            {
+                'id': product.id,
+                'name': product.name,
+                'price': product.price,
+                'page_number': product.page_number,
+                'site': product.site.name,
+                'brand': product.brand.name,
+                'product_category': product.product_category.name,
+                'product_type': product.product_type.name if product.product_type else None
+            } for product in products
+        ]
+        return self.get_paginated_response(data)
+
 
 class ProductDetailResource(Resource):
     def get(self, product_id):
